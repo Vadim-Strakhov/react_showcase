de9cacdf936b1aa937aa2f46b80c273ce86766f2
Vadim-Strakhov <mars1597@gmail.com>
2023-11-04 12:12:34 +0300

    fixed props

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -1,43 +1,27 @@
 import React from 'react';
 
 export const GoodsItem = (props) => {
-    const {
-        mainId,
-        displayName,
-        displayDescription,
-        displayAssets,
-        price,
-        addToBasket = Function.prototype,
-    } = props;
+    console.log('Props received in GoodsItem:', props);
+    const { mainId, displayName, displayDescription, price, granted, addToBasket = Function.prototype } = props;
 
     const finalPrice = price.finalPrice;
-    const background = displayAssets[0].background;
+    const background = granted[0].images.full_background;
+    // const background = displayAssets[0].background;
 
     return (
         <div className='card'>
             <div className='card-image'>
-                <img
-                    src={background}
-                    alt={displayName}
-                />
+                <img src={background} alt={displayName} />
             </div>
             <div className='card-content'>
                 <span className='card-title'>{displayName}</span>
                 <p>{displayDescription}</p>
             </div>
             <div className='card-action'>
-                <button
-                    className='btn'
-                    onClick={() =>
-                        addToBasket({ mainId, displayName, finalPrice })
-                    }
-                >
+                <button className='btn' onClick={() => addToBasket({ mainId, displayName, finalPrice })}>
                     Купить
                 </button>
-                <span
-                    className='right'
-                    style={{ fontSize: '1.8rem' }}
-                >
+                <span className='right' style={{ fontSize: '1.8rem' }}>
                     {finalPrice} руб
                 </span>
             </div>
